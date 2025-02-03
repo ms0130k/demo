fe6221ecfe9c39c2127a7ce1ac35fb327e5aafbe
KimDonghyeon <minseok0130@gmail.com>
2025-02-03 21:52:51 +0900

    init

---
+++ src/main/java/com/example/demo/controller/RestApiDemoController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.Coffee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/")
+public class RestApiDemoController {
+    private List<Coffee> coffees = new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Cafe Cereza"),
+                new Coffee("Cafe Ganador"),
+                new Coffee("Cafe Lareno"),
+                new Coffee("Cafe Tres Pontas")
+        ));
+
+    }
+
+    @GetMapping("/coffees")
+    public List<Coffee> getCoffees() {
+        return coffees;
+    }
+
+    @GetMapping("/coffees/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        return coffees.stream()
+                .filter(coffee -> id.equals(coffee.getId()))
+                .findFirst();
+    }
+
+    @PostMapping("/coffees")
+    public Coffee postCoffee(@RequestBody Coffee coffee) {
+        coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping("/coffees/{id}")
+    public Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
+        Optional<Coffee> coffeeOptional = getCoffeeById(id);
+        if (coffeeOptional.isPresent()) {
+            Coffee existingCoffee = coffeeOptional.get();
+            existingCoffee.setName(coffee.getName());
+            return existingCoffee;
+        } else {
+            coffee.setId(id);
+            coffees.add(coffee);
+            return coffee;
+        }
+    }
+
+    @DeleteMapping("/coffees/{id}")
+    public void deleteCoffee(@PathVariable String id) {
+        coffees.removeIf(coffee -> id.equals(coffee.getId()));
+    }
+}
---
+++ src/main/java/com/example/demo/domain/Coffee.java
@@ -0,0 +1,32 @@
+package com.example.demo.domain;
+
+import java.util.UUID;
+
+public class Coffee {
+    private String id;
+    private String name;
+
+    public Coffee() {
+    }
+
+    public Coffee(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
